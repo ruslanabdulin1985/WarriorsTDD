a0b594926be636b502fd6943f7ce23b8f0196893
Ruslan <ruslanabdulin1985@gmail.com>
2019-11-27 04:42:07 +0100

    :::REFACTOR::: FightTest

--- src/tests/FightTest.java
+++ src/tests/FightTest.java
@@ -21,32 +21,30 @@ public class FightTest {
 	
 	
 	@Test
-	void attackTest() {
-		Part sharedPart = mock(Part.class);
+	void whenAttackWariorStrikeIsExecutedOnce() {
 		Warrior enemy = mock(Warrior.class);
 		Warrior warrior = mock(Warrior.class);
 		
 		Fight sut = new Fight(warrior, enemy);
 		sut.atack();
 		
-		verify(warrior, atLeast(1)).strike(enemy, 5);
-		verify(warrior, atMost(1)).strike(enemy, 5);
+		verify(warrior).strike(enemy, 5);
 	}
 	
 	@Test
-	void deffendTest() {	
+	void whenDeffendEnemyStrikeIsExecutedOnce() {	
 		Warrior enemy = mock(Warrior.class);
 		Warrior warrior = mock(Warrior.class);
 		
 		Fight sut = new Fight(warrior, enemy);
 		sut.defend();
 		
-		verify(enemy, atLeast(1)).strike(warrior, 5);
-		verify(enemy, atMost(1)).strike(warrior, 5);
+		verify(enemy).strike(warrior, 5);
+
 	}
 	
 	@Test
-	void setBlockTestPlayer() {
+	void whenSetBlockPlayerBlockIsExectuedOnce() {
 		Part somePart1 = mock(Part.class);
 		Part somePart2 = mock(Part.class);
 		
@@ -56,19 +54,13 @@ public class FightTest {
 		Fight sut = new Fight(warrior, enemy);
 		sut.setBlockWarrior(somePart1, somePart2);
 		
-		verify(warrior, atLeast(1)).block(somePart1, somePart2);
-		verify(warrior, atMost(1)).block(somePart1, somePart2);
-	
-//		sut.setBlockEnemy();
-//
-//		verify(enemy, atLeast(1)).block(somePart1, somePart2);
-//		verify(enemy, atMost(1)).block(somePart1, somePart2);
+		verify(warrior).block(somePart1, somePart2);
+
 	}
 	
 	@Test
-	void setBlockTestEnemy() {
+	void whenSetBlockEnemyBlocIsExecutedOnce() {
 		Part somePart1 = mock(Part.class);
-		Part somePart2 = mock(Part.class);
 		
 		Warrior enemy = mock(Warrior.class);
 		Warrior warrior = mock(Warrior.class);
@@ -78,8 +70,6 @@ public class FightTest {
 		Fight sut = new Fight(warrior, enemy);
 		sut.setBlockEnemy();
 
-		verify(enemy, atLeast(1)).block(somePart1, somePart1);
-		verify(enemy, atMost(1)).block(somePart1, somePart1);
-	
+		verify(enemy).block(somePart1, somePart1);
 	}
 }
